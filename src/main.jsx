cd0a637f1480fda629acfd729220f1c1b9c12753
Baran Taşcı <barantasci44@gmail.com>
2026-05-12 11:52:24 +0300

    initial vite setup and project structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { ThemeProvider } from "styled-components";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { store, persistor } from "~/store/store.js";
+import { theme } from "~/styles/theme.js";
+import { GlobalStyle } from "~/styles/GlobalStyle.js";
+import { Toaster } from "~/components/Toaster/Toaster.jsx";
+import { App } from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/e-pharmacy">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Toaster />
+            <App />
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
