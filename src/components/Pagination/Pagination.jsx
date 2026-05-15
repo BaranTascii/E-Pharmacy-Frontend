0a5658fd80258407bb2f58a5de79cf4650bd852e
Baran Taşcı <barantasci44@gmail.com>
2026-05-15 13:53:35 +0300

    improve reusable pagination component

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,13 +1,23 @@
-const Pagination = () => {
+import clsx from "clsx";
+import s from "./Pagination.module.css";
+
+const Pagination = ({ totalPages, currentPage, handleChangePage }) => {
   return (
-    <div className="pagination">
-      <button>{"<"}</button>
-
-      <button className="active-page">1</button>
-
-      <button>{">"}</button>
-    </div>
+    totalPages > 1 && (
+      <ul className={s.paginationList}>
+        {Array.from({ length: totalPages }, (_, i) => {
+          const page = i + 1;
+          return (
+            <li className={s.paginationItem} key={page}>
+              <button
+                className={clsx(s.btn, page === currentPage && s.active)}
+                onClick={() => handleChangePage(page)}
+              ></button>
+            </li>
+          );
+        })}
+      </ul>
+    )
   );
 };
-
 export default Pagination;
