6e1102b2127aaff72a59f625f8525206d92d63f3
Baran Taşcı <barantasci44@gmail.com>
2026-05-17 22:56:48 +0300

    improve responsive layout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,29 +1,26 @@
-import { Outlet } from "react-router-dom";
-import { useState } from "react";
-
-import Header from "../Header/Header";
-import Sidebar from "../Sidebar/Sidebar";
+import Header from '../Header/Header.jsx';
+import Sidebar from '../Sidebar/Sidebar.jsx';
+import useResponsive from '../../hooks/useResponsive.js';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import Container from '../Container/Container.jsx';
+import s from './SharedLayout.module.css';
 
 const SharedLayout = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen((prev) => !prev);
-  };
+  const { isDesktop } = useResponsive();
 
   return (
-    <div className="layout">
-      <Sidebar isOpen={isSidebarOpen} />
-
-      <div className="layout-content">
-        <Header toggleSidebar={toggleSidebar} />
-
-        <main className="main-content">
-          <Outlet />
-        </main>
-      </div>
+    <div className={s.mainContainer}>
+      <Header />
+      {isDesktop && <Sidebar />}
+      <main className={s.main}>
+        <Container>
+          <Suspense fallback={<h3>Loading...</h3>}>
+            <Outlet />
+          </Suspense>
+        </Container>
+      </main>
     </div>
   );
 };
-
 export default SharedLayout;
