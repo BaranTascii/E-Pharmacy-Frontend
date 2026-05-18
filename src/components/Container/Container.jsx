88fa6b548c18708cfcd0c5918384f5049a030625
Baran Taşcı <barantasci44@gmail.com>
2026-05-18 14:12:01 +0300

    update Container and Logo

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,16 @@
-const Container = ({ children }) => {
-  return <div className="container">{children}</div>;
-};
+import { useLocation } from "react-router-dom";
+import useResponsive from "../../hooks/useResponsive.js";
+import s from "./Container.module.css";
 
+const Container = ({ children }) => {
+  const { isDesktop } = useResponsive();
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+
+  return (
+    <div className={isDesktop && isLoginPage ? s.loginContainer : s.container}>
+      {children}
+    </div>
+  );
+};
 export default Container;
