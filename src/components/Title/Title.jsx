03141e08a8561b0539a26a4ac2a90f9bae056d0b
Baran Taşcı <barantasci44@gmail.com>
2026-05-18 14:15:05 +0300

    update Title

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,5 +1,6 @@
-const Title = ({ children }) => {
-  return <h1 className="page-title">{children}</h1>;
-};
+import s from "./Title.module.css";
 
+const Title = () => {
+  return <p className={s.title}>Medicine store</p>;
+};
 export default Title;
