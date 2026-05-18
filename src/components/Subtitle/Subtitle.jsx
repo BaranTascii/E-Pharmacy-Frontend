6bf5149e1341830d22b4b8d83f625bc3cfd198a1
Baran Taşcı <barantasci44@gmail.com>
2026-05-18 14:14:27 +0300

    update Subtitle

--- src/components/Subtitle/Subtitle.jsx
+++ src/components/Subtitle/Subtitle.jsx
@@ -1,5 +1,22 @@
-const Subtitle = ({ children }) => {
-  return <p className="page-subtitle">{children}</p>;
-};
+import { useLocation } from "react-router-dom";
+import s from "./Subtitle.module.css";
 
+const Subtitle = () => {
+  const location = useLocation();
+  const dashboard = location.pathname === "/dashboard" && "Dashboard";
+  const allOrders = location.pathname === "/orders" && "All orders";
+  const allProducts = location.pathname === "/products" && "All products";
+  const allSuppliers = location.pathname === "/suppliers" && "All suppliers";
+  const allCustomers = location.pathname === "/customers" && "All customers";
+
+  return (
+    <div className={s.subtitleContainer}>
+      <p className={`${s.subtitle} ${s.path}`}>
+        {dashboard || allOrders || allProducts || allSuppliers || allCustomers}
+      </p>
+      <p className={s.subtitle}>|</p>
+      <p className={s.subtitle}>vendor@gmail.com</p>
+    </div>
+  );
+};
 export default Subtitle;
