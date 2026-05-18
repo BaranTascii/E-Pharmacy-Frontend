29c9d585adfbd1261807020c081452113442a6ad
Baran Taşcı <barantasci44@gmail.com>
2026-05-18 14:10:52 +0300

    update BurgerMenu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,11 +1,68 @@
-import { HiOutlineMenuAlt2 } from "react-icons/hi";
+import { useEffect, useState } from "react";
+import sprite from "../../assets/sprite.svg";
+import LogoutBtn from "../LogoutBtn/LogoutBtn.jsx";
+import SidebarMenu from "../SidebarMenu/SidebarMenu.jsx";
+import { noScroll } from "../../utils/noScroll.js";
+import s from "./BurgerMenu.module.css";
 
-const BurgerMenu = ({ onClick }) => {
+const BurgerMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    noScroll(isMenuOpen);
+  }, [isMenuOpen]);
+
+  const handleCloseMenu = (e) => {
+    e.stopPropagation();
+    if (e.target !== e.currentTarget) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  const handleClickOutside = (e) => {
+    if (e.target === e.currentTarget) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleKeydown = (e) => {
+      if (e.code === "Escape") {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeydown);
+    return () => {
+      document.removeEventListener("keydown", handleKeydown);
+    };
+  }, [setIsMenuOpen]);
+
   return (
-    <button onClick={onClick} className="burger-btn">
-      <HiOutlineMenuAlt2 size={24} />
-    </button>
+    <>
+      <button
+        className={s.burgerMenu}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <svg width={32} height={32}>
+          <use href={`${sprite}#icon-menu-burger`}></use>
+        </svg>
+      </button>
+
+      {isMenuOpen && (
+        <div className={s.menuOverlay} onClick={handleClickOutside}>
+          <div className={s.menu} onClick={handleCloseMenu}>
+            <button className={s.btn} type="button" onClick={handleCloseMenu}>
+              <svg width={32} height={32}>
+                <use href={`${sprite}#icon-close`}></use>
+              </svg>
+            </button>
+            <SidebarMenu />
+            <LogoutBtn />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
-
 export default BurgerMenu;
