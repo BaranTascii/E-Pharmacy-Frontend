709dba88756a0b0d56c143f7907e563633896981
Baran Taşcı <barantasci44@gmail.com>
2026-05-18 14:13:30 +0300

    update ProductNameFilter

--- src/components/ProductNameFilter/ProductNameFilter.jsx
+++ src/components/ProductNameFilter/ProductNameFilter.jsx
@@ -1,13 +1,34 @@
-const ProductNameFilter = ({ value, onChange }) => {
+import sprite from "../../assets/sprite.svg";
+import { useForm } from "react-hook-form";
+import s from "./ProductNameFilter.module.css";
+
+const ProductNameFilter = ({ handleChangeFilterName }) => {
+  const { register, handleSubmit, reset, watch } = useForm({
+    mode: "onSubmit",
+  });
+
+  const query = watch("search");
+
+  const onSubmit = () => {
+    handleChangeFilterName(query);
+    reset();
+  };
+
   return (
-    <input
-      type="text"
-      placeholder="Search product..."
-      value={value}
-      onChange={onChange}
-      className="product-filter-input"
-    />
+    <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("search")}
+        className={s.input}
+        type="text"
+        placeholder="Product Name"
+      />
+      <button className={s.btn} type="submit">
+        <svg className={s.iconBtn} width={14} height={14}>
+          <use href={`${sprite}#icon-filter`}></use>
+        </svg>
+        Filter
+      </button>
+    </form>
   );
 };
-
 export default ProductNameFilter;
